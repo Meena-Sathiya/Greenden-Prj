6f45aac7bdf3055c8948b99966cd0ceee6e5e947
Meena-Sathiya <meegalsathiya98@gmail.com>
2025-08-06 21:23:43 +0100

    push prj

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+//my tutor did without this file it shows suggestion fr me it dind wrk i create this file give this code now i got suggestion/ /
+
+module.exports = {
+    content: ["./*.html"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
